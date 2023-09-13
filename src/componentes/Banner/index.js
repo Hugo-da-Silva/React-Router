c537d39cf9cfa66a4573fecde16df9249548d646
Hugo da Silva <hdasilva97@gmail.com>
2023-09-12 21:15:51 -0300

    Terceiro Commit
    terceiro Commit, pasta src ja editada e alguns ajustes em <img/> e <p>

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,6 +1,6 @@
 import styles from './Banner.module.css';
 import circuloColorido from 'assets/circulo_colorido.png';
-import minhaFoto from 'assets/minha_foto.png'
+import minhaFoto from 'assets/minha-foto-Hugo.jfif'
 
 export default function Banner() {
     return (
@@ -11,12 +11,13 @@ export default function Banner() {
                 </h1>
 
                 <p className={styles.paragrafo}>
-                    Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+                    Boas vindas ao meu espaço pessoal! Eu sou Hugo da Silva, Aluno de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda sempre algo novo :)
                 </p>
             </div>
 
             <div className={styles.imagens}>
                 <img
+                    alt='Circulo colorido da foto'
                     className={styles.circuloColorido}
                     src={circuloColorido}
                     aria-hidden={true}
@@ -25,7 +26,7 @@ export default function Banner() {
                 <img
                     className={styles.minhaFoto}
                     src={minhaFoto}
-                    alt="Foto do Antônio Evaldo sorrindo"
+                    alt="Foto do Hugo da silva"
                 />
             </div>
         </div>
